8807daf6bc91dc1ec9e8347e4f4dd7e3b112a08d
Masanori Tanaka <yuui.tanaka@gmail.com>
2017-02-26 10:01:26 +0900

    completed Chapter 4

--- web/router.ex
+++ web/router.ex
@@ -15,8 +15,8 @@ defmodule Rumbl.Router do
 
   scope "/", Rumbl do
     pipe_through :browser # Use the default browser stack
-
     get "/", PageController, :index
+    resources "/users", UserController, only: [:index, :show, :new, :create]
   end
 
   # Other scopes may use custom stacks.
